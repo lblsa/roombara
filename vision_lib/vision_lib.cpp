6702f6fb782ce005dba0a69daa91dfd58ee3ae05
Vladimir Stasevich <vladimir.stasevich@gmail.com>
2013-02-13 02:00:57 +0400

    Vision lib creation - static lib for all Computer Vision tools for Roombara project

---
+++ vision_lib/vision_lib.cpp
@@ -0,0 +1,12 @@
+/*
+ * vision_lib.cpp
+ *
+ *  Created on: 13.02.2013
+ *      Author: Tatyana
+ */
+
+#include "vision_lib.h"
+
+RbVision::RbVision()
+{
+}
---
+++ vision_lib/vision_lib.h
@@ -0,0 +1,18 @@
+/*
+ * vision_lib.h
+ *
+ *  Created on: 13.02.2013
+ *      Author: Tatyana
+ */
+
+#ifndef VISION_LIB_H_
+#define VISION_LIB_H_
+
+class RbVision
+{
+public:
+   RbVision();
+};
+
+
+#endif /* VISION_LIB_H_ */
